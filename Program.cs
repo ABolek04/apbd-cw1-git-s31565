32072cb1912f30ba0ae00d1b0737558c573f8b2d
Adam Belowski <belowskiadam02@gmail.com>
2026-03-18 20:29:21 +0100

    Add Print student name in console

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
-﻿public class Program
+﻿namespace DefaultNamespace;
+
+public class Program
 {
     public static void Main()
     {
         Student s = new Student();
         s.ID = 10;
         s.Name = "Adam";
+        Console.WriteLine(s.Name);
     }
 }
+
+
