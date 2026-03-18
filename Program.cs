7321488c9c6a55e81a504ed49f228fa89accf3ea
Adam Belowski <belowskiadam02@gmail.com>
2026-03-18 20:42:04 +0100

    Add default constructor to Student class

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ public class Program
         s.ID = 10;
         s.Name = "Adam";
         Console.WriteLine(s.Name);
+        Student s2 = new Student(15,"Adam2");
+        Console.WriteLine(s2.ID);
     }
 }
 
--- Student.cs
+++ Student.cs
@@ -2,6 +2,16 @@
 
 public class Student
 {
+    public Student(int ID, string Name)
+    {
+        this.ID = ID;
+        this.Name = Name;
+    }
+
+    public Student()
+    {
+        
+    }
     public int ID { get; set; }
     public string Name { get; set; }
 }
